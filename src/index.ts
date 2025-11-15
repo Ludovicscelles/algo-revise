0a674a89b99f868c4b1ce84ef660ac001abf1efb
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-15 18:34:19 +0100

    index: revise function evenOrOdd v2 (returns a string)

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ function evenOrOdd(nbr: number): boolean {
 
   if (nbr % 2 !== 0) {
     return false;
-
   }
   return true;
 }
@@ -21,3 +20,26 @@ function evenOrOdd(nbr: number): boolean {
 let nbr = 15;
 let isEven = evenOrOdd(nbr);
 console.log(isEven);
+
+{
+  /*
+
+  1bis. Pair ou impair
+Écris un programme qui prend un nombre entier et affiche s’il est pair ou impair.
+  */
+}
+
+function evenOrOdd2(nbr2: number): string {
+  if (!Number.isInteger(nbr2)) {
+    throw new Error(`La saisie doit être un nombre entier`);
+  }
+
+  if (nbr2 % 2 !== 0) {
+    return `Ce nombre est impair`;
+  }
+  return `Ce nombre est pair`;
+}
+
+let nbr2 = 27;
+let isEven2: string = evenOrOdd2(nbr2);
+console.log(isEven2);
