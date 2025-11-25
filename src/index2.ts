9ccd88416e5751fbc4a0258b10f14aea0928c125
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-25 15:45:28 +0100

    index2: revise function getDigitCount3 using a while loop

--- src/index2.ts
+++ src/index2.ts
@@ -47,3 +47,30 @@ function getDigitCount2(int2: unknown): number {
 const int2 = 1991;
 const digitCount2: number = getDigitCount2(int2);
 console.log(digitCount2);
+
+/*
+  1v3. Nombre de chiffres
+Écris une fonction qui retourne le nombre de chiffres dans un entier donné.
+Exemple : 12345 → 5
+Boucle while.
+*/
+
+function getDigitCount3(int3: unknown): number {
+  checkIsInteger(int3);
+
+  let strInt3: string = Math.abs(int3).toString();
+
+  let count3: number = 0;
+
+  let i = 0;
+
+  while (i < strInt3.length) {
+    count3++;
+    i++;
+  }
+  return count3;
+}
+
+const int3 = 222332;
+const digitCount3: number = getDigitCount3(int3);
+console.log(digitCount3);
