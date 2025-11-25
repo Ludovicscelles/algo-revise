1bfe1dcd2a3f599d662c5ca300311231abcdd387
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-25 15:41:50 +0100

    index2: revise function getDigitCount2 using a reverse for loop

--- src/index2.ts
+++ src/index2.ts
@@ -1,11 +1,10 @@
+import { func } from "joi";
 import { checkIsInteger } from "./CheckFunctions";
 
 /*
   1. Nombre de chiffres
 Écris une fonction qui retourne le nombre de chiffres dans un entier donné.
-
 Exemple : 12345 → 5
-
 Boucle for.
 */
 
@@ -24,3 +23,27 @@ function getDigitCount(int: unknown): number {
 const int = 12345;
 const digitCount: number = getDigitCount(int);
 console.log(digitCount);
+
+/*
+  1v2. Nombre de chiffres
+Écris une fonction qui retourne le nombre de chiffres dans un entier donné.
+Exemple : 12345 → 5
+Boucle for inversé.
+*/
+
+function getDigitCount2(int2: unknown): number {
+  checkIsInteger(int2);
+
+  const strInt2: string = Math.abs(int2).toString();
+
+  let count2: number = 0;
+
+  for (let i = strInt2.length - 1; i >= 0; i--) {
+    count2++;
+  }
+  return count2;
+}
+
+const int2 = 1991;
+const digitCount2: number = getDigitCount2(int2);
+console.log(digitCount2);
