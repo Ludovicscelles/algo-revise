afeb425cb932e283187ee7bee3dec339e6a04cc5
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-11-25 15:37:20 +0100

    index2: revise function getDigitCount using a for loop

--- src/index2.ts
+++ src/index2.ts
@@ -1,9 +1,26 @@
 import { checkIsInteger } from "./CheckFunctions";
 
-  /*
+/*
   1. Nombre de chiffres
 Écris une fonction qui retourne le nombre de chiffres dans un entier donné.
 
 Exemple : 12345 → 5
+
+Boucle for.
 */
 
+function getDigitCount(int: unknown): number {
+  checkIsInteger(int);
+
+  const strInt: string = Math.abs(int).toString();
+  let count: number = 0;
+
+  for (let i = 0; i < strInt.length; i++) {
+    count++;
+  }
+  return count;
+}
+
+const int = 12345;
+const digitCount: number = getDigitCount(int);
+console.log(digitCount);
