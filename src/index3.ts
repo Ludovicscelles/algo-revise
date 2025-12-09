2ecc7ed8601f694d9b43adeb8114b6a024762354
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2025-12-09 17:19:32 +0100

    index3: revise function evenDigitsSum2 using a reverse for loop

--- src/index3.ts
+++ src/index3.ts
@@ -9,7 +9,6 @@ Boucle for.
 */
 }
 
-
 function evenDigitsSum(integer: unknown): number {
   checkIsInteger(integer);
 
@@ -27,3 +26,30 @@ function evenDigitsSum(integer: unknown): number {
 const integer = 2021;
 const sumOfEvenDigits = evenDigitsSum(integer);
 console.log(sumOfEvenDigits);
+
+{
+  /*
+  🔢 1v2. Somme des chiffres pairs
+Écris une fonction qui prend un nombre entier et retourne la somme de ses chiffres pairs.
+📌 Exemple : 2489 → 2 + 4 + 8 = 14
+Boucle for inversé.
+*/
+}
+
+function evenDigitsSum2(integer: unknown): number {
+  checkIsInteger(integer);
+
+  const integerAsString: string = Math.abs(integer).toString();
+
+  let sumOfEvenDigits: number = 0;
+
+  for (let i = integerAsString.length - 1; i >= 0; i--) {
+    const digit: number = parseInt(integerAsString[i]);
+    if (digit % 2 === 0) sumOfEvenDigits += digit;
+  }
+  return sumOfEvenDigits;
+}
+
+const integer2 = -2489;
+const sumOfEvenDigits2 = evenDigitsSum2(integer2);
+console.log(sumOfEvenDigits2);
