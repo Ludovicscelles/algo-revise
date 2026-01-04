eb29ba75a7d1cdf0d56562da33df0e68d9044959
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-01-04 20:32:08 +0100

    fix(test): fix algo countVowels

--- src/algo/countVowels.ts
+++ src/algo/countVowels.ts
@@ -4,11 +4,10 @@ import { vowels } from "../utils/Normalize";
 export function countVowels(string: unknown): number {
   checkIsString(string);
 
-  const str = string.normalize("NFC");
   let count: number = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
       count++;
     }
   }
@@ -18,11 +17,10 @@ export function countVowels(string: unknown): number {
 export function countVowels7(string7: unknown): number {
   checkIsString(string7);
 
-  const str = string7.normalize("NFC");
   let count: number = 0;
 
-  for (let char of str) {
-    if (vowels.includes(char)) {
+  for (let str of string7) {
+    if (vowels.includes(str)) {
       count++;
     }
   }
